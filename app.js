5501a02c3e3750bd280e615a69f88be735b6046e
Malikaarjun <111501472+Malikaarjun@users.noreply.github.com>
2026-08-12 15:24:08 +0530

    Implement HTTP server for login page
    Added a simple HTTP server that serves a login page.

--- app.js
+++ app.js
@@ -1,2 +1,23 @@
-const message = "Login feature";
-console.log(message);
+const http = require('http');
+ 
+const server = http.createServer((req, res) => {
+  res.writeHead(200, { 'Content-Type': 'text/html' });
+ 
+  res.end(`
+    <html>
+      <head>
+        <title>Login Page</title>
+      </head>
+      <body>
+        <h1>Login Page</h1>
+        <input id="username" placeholder="Username" />
+        <input id="password" type="password" placeholder="Password" />
+        <button id="login">Login</button>
+      </body>
+    </html>
+  `);
+});
+ 
+server.listen(3000, () => {
+  console.log('Application running on port 3000');
+});
