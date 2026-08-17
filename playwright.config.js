ffe14059f8243eb16ab9e52534d323e5abcd0dfa
Malikaarjun <111501472+Malikaarjun@users.noreply.github.com>
2026-08-17 09:59:38 +0000

    Add playwright HTML report to CI

--- playwright.config.js
+++ playwright.config.js
@@ -2,6 +2,21 @@ const { defineConfig } = require('@playwright/test');
  
 module.exports = defineConfig({
   testDir: './tests',
+
+  reporter:[
+    ['list'],
+    ['html', {
+      outputFolder: 'playwright-report',
+      open: 'never'
+    }]
+  ],
+
+  use: {
+    headless: true,
+    trace: 'on-first-retry',
+    screenshot: 'only-on-failure',
+    video: 'retain-on-failure'
+  },
  
   webServer: {
     command: 'node app.js',
